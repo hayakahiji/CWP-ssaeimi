09f957b40b3ff9ae35733aa92ba58411c5c1fa61
hayakahiji <hayakahiji@gmail.com>
2026-09-22 13:18:08 +0700

    add cell04,ex02

---
+++ cell04/ex02/calculator.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+first_num = int(input("Give me the first number: "))
+second_num = int(input("Give me the second number: "))
+
+print("Thank you!")
+
+print(f"{first_num} + {second_num} = {first_num + second_num}")
+print(f"{first_num} - {second_num} = {first_num - second_num}")
+print(f"{first_num} / {second_num} = {first_num / second_num}")
+print(f"{first_num} * {second_num} = {first_num * second_num}")
