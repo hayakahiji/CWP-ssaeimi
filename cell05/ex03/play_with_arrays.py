e994caa2cf0742ad6293618039f8e0d9eae2c284
hayakahiji <hayakahiji@gmail.com>
2026-09-22 16:24:09 +0700

    add cell05,ex03

---
+++ cell05/ex03/play_with_arrays.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+ori_arr = [2,8,9,48,8,22,-12,2]
+new_arr = []
+
+
+print(f"{ori_arr}")
+
+
+i = 0
+
+while i < len(ori_arr):
+    
+    ori_arr[i] += 2
+    if ori_arr[i] > 5:
+        new_arr.append(ori_arr[i])
+    i+=1
+
+
+
+print(f"{set(new_arr)}")
