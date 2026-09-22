752513ea2e90a56b0030b2c863b001ccde8fa71f
hayakahiji <hayakahiji@gmail.com>
2026-09-22 15:00:47 +0700

    add cell05,ex01

---
+++ cell05/ex01/play_with_arrays.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+ori_arr = [2,8,9,48,8,22,-12,2]
+
+print(f"Original array: {ori_arr}")
+
+
+i = 0
+
+while i < len(ori_arr):
+    ori_arr[i] += 2
+    i+=1
+
+
+
+print(f"New array: {ori_arr}")
