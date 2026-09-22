bfd10eef7e7bf9b4b0e2b29931b2df29b5e9075f
hayakahiji <hayakahiji@gmail.com>
2026-09-22 18:15:40 +0700

    add cell05,ex11

---
+++ cell05/ex11/count_it.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+import sys
+
+if len(sys.argv) == 1:
+    print("none")
+else:
+    params = sys.argv[1:]
+    print(f"parameters: {len(params)}")
+    for p in params:
+        print(f"{p}: {len(p)}")
