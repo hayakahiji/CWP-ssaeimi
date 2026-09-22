0e6eb6507c5dd1bfbf9f98feb7a54f91ed401623
hayakahiji <hayakahiji@gmail.com>
2026-09-22 13:11:14 +0700

    add cell04,ex01

---
+++ cell04/ex01/age.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+text = input("Please tell me your age: ")
+
+print(f"You are currently {text.strip()} years old.")
+print(f"In 10 years, you'll be {int(text.strip()) + 10} years old.")
+print(f"In 20 years, you'll be {int(text.strip()) + 20} years old.")
+print(f"In 30 years, you'll be {int(text.strip()) + 30} years old.")
