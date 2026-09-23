2745f24d2496c2845abd03f0302c345ef9f607a3
Yeennawa <nwminthrchuncitr78@gmail.com>
2026-09-23 22:53:11 +0700

    add checkmate

--- miniproject/ex00/main.py
+++ miniproject/ex00/main.py
@@ -1,2 +1,14 @@
 #!/usr/bin/env python3
+from checkmate import checkmate
+from sys import argv
+def main():
+    try:
+        with open(argv[1], "r") as file:
+            board = file.read()
 
+        checkmate(board)
+
+    except (FileNotFoundError, IndexError):
+        return
+if __name__ == "__main__":
+    main()
